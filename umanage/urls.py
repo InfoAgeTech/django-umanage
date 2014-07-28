eb3e598df37f5f922253b8f1f241bba7f383edae
Troy Grosfield <troy.grosfield@gmail.com>
2014-07-28 16:36:45 -0600

    cleaned up unnecessary content.

--- umanage/urls.py
+++ umanage/urls.py
@@ -4,9 +4,6 @@ from django.conf.urls import include
 
 
 urlpatterns = patterns('',
-#     url(r'^/add/?$', SlideAssetAddView.as_view(), name='slide_asset_add'),
     url(r'', include('umanage.change_email.urls')),
     url(r'', include('umanage.change_password.urls')),
-#     url(r'^/(?P<slide_asset_id>\d+)/?$', SlideAssetView.as_view(), name='slide_asset_view'),
-#     url(r'^/(?P<slide_asset_id>\d+)/edit/?$', SlideAssetEditView.as_view(), name='slide_asset_edit'),
 )
