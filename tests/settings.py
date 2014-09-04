3bb4bdad37ffa44168e4230dca364f014577d9da
Troy Grosfield <troy.grosfield@gmail.com>
2014-09-04 17:18:39 -0600

    minor manage.py code update

--- tests/settings.py
+++ tests/settings.py
@@ -5,28 +5,21 @@ import sys
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['*']
+LANGUAGE_CODE = 'en-us'
+ROOT_URLCONF = 'urls'
+SECRET_KEY = '12345abcd'
+SITE_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')
+SITE_ID = 1
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+
 UMANAGE_BASE_TEMPLATE = 'base_umanage.html'
 UMANAGE_BASE_UNAUTHENTICATED_TEMPLATE = 'base_umanage_unauthenticated.html'
 UMANAGE_FROM_EMAIL = 'noreply@example.com'
 UMANAGE_SITE_ROOT_URI = 'http://somedomain.com'
 UMANAGE_SITE_NAME = 'My Site Name'
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'UTC'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -51,21 +44,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'umanage.context_processors.common',
 )
 
-SITE_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')
 TEMPLATE_DIRS = (
     os.path.join(SITE_ROOT, 'tests/templates'),
     os.path.join(SITE_ROOT, 'umanage/templates'),
 )
-
-ROOT_URLCONF = 'urls'
-
-# Added in django 1.5 secret key is required.  This is a random generated string
-SECRET_KEY = '12345abcd'
-
-# Added in django 1.4.4. See: https://docs.djangoproject.com/en/1.4/releases/1.4.4/#host-header-poisoning
-ALLOWED_HOSTS = ['*']
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 here = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
